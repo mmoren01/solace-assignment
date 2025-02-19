08ae66f54c95145c3238dd9f12e1e79bec051dfa
Mauricio Moreno <mmoren01@protonmail.com>
2025-02-18 19:23:58 -0700

    Remove commented-out code in GET endpoint for advocates

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,12 +1,9 @@
 import db from "../../../db";
 import { advocates } from "../../../db/schema";
 import { ilike, or } from "drizzle-orm";
-// import { advocateData } from "../../../db/seed/advocates";
 
 export async function GET() {
   const data = await db.select().from(advocates);
-
-  // const data = advocateData;
 
   return Response.json({ data });
 }
