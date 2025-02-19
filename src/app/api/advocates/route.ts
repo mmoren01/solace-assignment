db432811930d3b835861df94d79b5c8729820b18
Mauricio Moreno <mmoren01@protonmail.com>
2025-02-18 19:18:52 -0700

    Add POST endpoint to search advocates by first or last name

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,12 +1,30 @@
 import db from "../../../db";
 import { advocates } from "../../../db/schema";
-import { advocateData } from "../../../db/seed/advocates";
+import { ilike, or } from "drizzle-orm";
+// import { advocateData } from "../../../db/seed/advocates";
 
 export async function GET() {
-  // Uncomment this line to use a database
   const data = await db.select().from(advocates);
 
   // const data = advocateData;
+
+  return Response.json({ data });
+}
+
+export async function POST(request: Request) {
+  const { searchTerm } = await request.json();
+
+  const query = db
+    .select()
+    .from(advocates)
+    .where(
+      or(
+        ilike(advocates.firstName, `%${searchTerm}%`),
+        ilike(advocates.lastName, `%${searchTerm}%`)
+      )
+    );
+
+  const data = await query;
 
   return Response.json({ data });
 }
